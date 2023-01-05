dc3ff004a62b47a076eab5b504dfea247f5b08c7
senemogluc <71778631+senemogluc@users.noreply.github.com>
2023-01-05 21:19:12 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+    <script src="https://kit.fontawesome.com/1796e4fe38.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/1796e4fe38.js" crossorigin="anonymous"></script>
+
+
+    <title> COURSELF</title>
+
+</head>  
+<body>
+    <?php include 'Navbar.php' ?>
+    <div class="container">
+    </div>
+
+    <section id="homepage">
+        <div id="black">
+
+        </div>
+
+        <div id="content">
+            <h2>COURSELF.</h2>
+            <br>
+            <p>Start learning with Courself for best prices! </p>
+
+        </div>
+    </section>
+
+    <section id="about">
+        <h3>About COURSELF</h3>
+        <div class="container">
+            <div id="left">
+                <h5 id="h5left">Who we are? What is our aim?<br>Our mission and vision.</br>
+                </h5>
+            </div>
+
+            <div id="right"></div>
+           
+            <p id="pright">
+                Courself is the best online learning platform that you can learn anything with suiatable prices. Our mission and vision are help students and instructors to learn and teach as free and easy.
+                </p>
+        </div>
+
+        <img src="images/0x0.jpeg" alt="img" class="img-fl">
+
+    </section>
+
+
+    <section id="team">
+
+        <div class="container">
+
+            <h3 id="teamhead">Team</h3>
+
+            <div class="col-left-right">
+                <img src="images/cagin.jpeg" alt="" class="img-fl oval">
+                <h4 class="team-member"> Çağın Senemoğlu</h4>
+                <p class="member">Project Lead. Computer Engineering. </p>
+                <div class="team-icon">
+                    <a href="#"><i class="fa-brands fa-github social"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                </div>
+            </div>
+
+            <div class="col-left-right">
+                <img src="images/selin.png" alt="" class="img-fl oval">
+                <h4 class="team-member">Selin Üçyıldız </h4>
+                <p class="member">Project Lead. Computer Engineering. </p>
+                <div class="team-icon">
+                    <a href="#"><i class="fa-brands fa-github social"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                </div>
+            </div>
+
+            <div class="col-left-right">
+                <img src="images/bilge.jpg" alt="" class="img-fl oval">
+                <h4 class="team-member"> Bilge Kılıç</h4>
+                <p class="member">Project Lead. Computer Engineering. </p>
+                <div class="team-icon">
+                    <a href="#"><i class="fa-brands fa-github social"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                </div>
+            </div>
+            <div class="col-left-right">
+                <img src="images/enes.png" alt="" class="img-fl oval">
+                <h4 class="team-member"> Enes Yıldız</h4>
+                <p class="member">Computer Engineering. </p>
+                <div class="team-icon">
+                    <a href="#"><i class="fa-brands fa-github social"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                </div>
+            </div>
+            <div class="col-left-right">
+                <img src="images/tuana.jpeg" alt="" class="img-fl oval">
+                <h4 class="team-member"> Tuana Selen Özhazday</h4>
+                <p class="member">Computer Engineering. </p>
+                <div class="team-icon">
+                    <a href="#"><i class="fa-brands fa-github social"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                </div>
+            </div>
+
+
+        </div>
+
+    </section>
+
+
+    <section id="contact">
+        <div class="container">
+            <h3 id="contacth3">Contact Us!</h3>
+
+            <div id="opak">
+                <div id="formgroup">
+                    <div id="leftform"> 
+                        <input type="text" name="name" placeholder="Name Surname" required class="form-control">
+                        <input type="text" name="tel" placeholder="Phone Number" required class="form-control">
+                    </div>
+                    <div id="rightform">
+                        <input type="email" name="email" placeholder="Email Adress" required class="form-control">
+                        <input type="text" name="content" placeholder="Content" required class="form-control">
+                    </div>
+                    <textarea name="mesaj" id="" cols="30" placeholder="Write Your Message" rows="10" class="form-control"></textarea>
+                    <input type="submit" value="send">
+                </div>
+               
+                <div id="address">
+                    <h4 id="header">Address:</h4>
+                    <p class="adresp">Huzur Mah.</p>
+                    <p class="adresp">Maslak Ayazağa Cd. No:4, 34396</p>
+                    <p class="adresp">MEF University</p>
+                    <p class="adresp">(0212) 395 36 95</p>
+                    <p class="adresp">Email: ucyildizs@mef.edu.tr</p>
+                </div>
+
+            </div>
+
+            <footer>
+                <div id="copyright">2023 | © All Rights Reserved.</div>
+                <div id="soc-footer">
+                    <a href="#"><i class="fa-brands fa-github social"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                
+                </div>
+            </footer>
+        </div>
+    </section>
+</body>
+</html>
